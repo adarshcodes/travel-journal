07eca73064a50c734014117cde422b6a23112e0a
Adarshcodes <adarsh.lrnr@gmail.com>
2022-01-30 22:31:05 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Sass/main.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<header className="App-header">
+				<NavBar />
+			</header>
 		</div>
 	);
 }
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class NavBar extends React.Component {
+	render() {
+		return (
+			<div className="navbar">
+				<i class="fad fa-globe-asia"></i>
+				<p className="navbar__logo-text">my travel journal.</p>
+			</div>
+		);
+	}
+}
+
+export default NavBar;
